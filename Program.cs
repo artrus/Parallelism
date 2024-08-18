fc8c009f1d2e1d6192adee722ed9624ae6e41c03
artrus <art_rus@bk.ru>
2024-08-19 00:37:22 +0300

    add find in path

--- Program.cs
+++ Program.cs
@@ -1,7 +1,55 @@
-﻿internal class Program
+﻿using Parallelism;
+using System.Diagnostics;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Stopwatch stopwatch = new Stopwatch();
+
+        //Чтение 3х файлов в раздельных задачах
+        stopwatch.Start();
+        Task<int> task1 =  Task.Run(() =>
+        {
+            int count = SpaceFinder.FindSpacesInFile(@"d:\Projects\OTUS\DZ6\TestFiles\File1.ini");
+            return count;
+        });
+        Task<int> task2 = Task.Run(() =>
+        {
+            int count = SpaceFinder.FindSpacesInFile(@"d:\Projects\OTUS\DZ6\TestFiles\File2.ini");
+            return count;
+        });
+        Task<int> task3 = Task.Run(() =>
+        {
+            int count = SpaceFinder.FindSpacesInFile(@"d:\Projects\OTUS\DZ6\TestFiles\File3.ini");
+            return count;
+        });
+
+        while (!(task1.IsCompleted && task2.IsCompleted && task3.IsCompleted)) { }  //ожидание завершения всех задач
+
+        stopwatch.Stop();
+        Console.WriteLine($"Spaces in file1 = {task1.Result}");
+        Console.WriteLine($"Spaces in file2 = {task2.Result}");
+        Console.WriteLine($"Spaces in file3 = {task3.Result}");
+        Console.WriteLine($"3 files time={stopwatch.ElapsedMilliseconds}");
+        Console.WriteLine();
+
+        //чтение каталога
+        SpaceFinder spaceFinder = new SpaceFinder();
+        spaceFinder.FileResult += DisplaySpaces;    //подписка на событие после выполнения каждой задачи
+
+        stopwatch.Restart();
+        stopwatch.Start();
+        spaceFinder.FindInPath(@"d:\Projects\OTUS\DZ6\TestCatalog\");
+
+        while (!spaceFinder.isComplete()) { }   //ожидание выполнения всех задач 
+
+        stopwatch.Stop();
+        Console.WriteLine($"Path time={stopwatch.ElapsedMilliseconds}");
+    }
+
+    private static void DisplaySpaces(string message)
+    {
+        Console.WriteLine(message);
     }
 }
---
+++ SpaceFinder.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parallelism
+{
+    public class SpaceFinder
+    {
+        public delegate void EventHandler(string message);
+        public event EventHandler? FileResult;
+
+        List<Task>? tasks;
+
+        public static int FindSpacesInFile(string path)
+        {
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string file = sr.ReadToEnd();
+                int count = file.Count(Char.IsWhiteSpace);
+                return count;
+            }
+        }
+
+        public void FindInPath(string path)     //поиск в папке
+        {
+            tasks = new List<Task>();   
+            DirectoryInfo dir = new DirectoryInfo(path);
+            var files = dir.GetFiles();
+
+            foreach (var file in files)
+            {
+                var task = Task.Run(() =>
+                {
+                    var count = FindSpacesInFile(path + "/" + file.Name);
+                    FileResult?.Invoke($"{file.Name} spaces count={count}");
+                });
+                tasks.Add(task);
+            }
+        }
+
+        public bool isComplete()   
+        {
+            bool result = true;
+            foreach(var task in tasks)
+            {
+                if (!task.IsCompleted)
+                    result = false;
+            }
+            return result;
+        }
+    }
+}
